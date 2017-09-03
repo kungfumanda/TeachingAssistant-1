0e6970eb10a2928bcedeb89bc855463fbfff817e
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:28:49 -0300

    alerta para CPF já cadastrado

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -21,6 +21,7 @@ export class AppComponent {
        this.aluno = {nome: "", cpf: "", email: "", login: ""};
      } else {
        this.aluno.cpf = "";
+       alert("Já existe um aluno com esse CPF");
      }
   }
 }
